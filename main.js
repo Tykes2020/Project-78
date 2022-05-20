c6d7e592b625f7947f8a5a584c9bd29930b13ee3
Tykes2020 <98526186+Tykes2020@users.noreply.github.com>
2022-05-20 19:39:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var images = ["https://i.postimg.cc/MKdhy06Z/family.jpg","Grandpa.jpeg", "Grandma.jpeg" , "Dad.jpeg", "Mom.jpeg", "Daughter.png", "Son.jpeg", "Daksh.jpeg"];
+var names = ["Family Book","Grandpa", "Grandma", "Daddy", "Mommy", "Daughter", "Son", "Brother"];
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 7
+    if(i > numbers_of_family_member_in_array)
+      {
+          i = 0;
+      }
+    var updatedImage = images[i];
+    var updatedName = names[i];
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
